39fe4406d801faa5174ce5cbfdc08e6beb05eea1
gauravpartha <gaurav.parthasarathy@inf.ethz.ch>
2023-02-06 20:53:26 +0100

    generate hints for if-statement

--- src/main/scala/viper/carbon/proofgen/hints/MLHintGenerator.scala
+++ src/main/scala/viper/carbon/proofgen/hints/MLHintGenerator.scala
@@ -9,8 +9,16 @@ object MLHintGenerator {
     stmtProofHint match {
       case LocalVarAssignHint(assignVpr, lhsBpl, hints) =>
         MLUtil.app("AssignHint", MLUtil.createTuple(Seq(expRelInfo, MLUtil.isaToMLThm(boogieProcAccessor.getLookupThyThm(lhsBpl)))))
-      case SeqnProofHint(hints, scopedDecls) => ???
-      case IfHint(cond, componentHints) => ???
+      case SeqnProofHint(hints, scopedDecls) =>
+        MLUtil.app("SeqnHint", MLUtil.createList(hints.map(hint => generateHintsInML(hint, boogieProcAccessor, expRelInfo))))
+      case IfHint(cond, componentHints) =>
+        componentHints match {
+          case Seq(IfComponentHint(thnHint, elsHint)) =>
+            val thnHintString = generateHintsInML(thnHint, boogieProcAccessor, expRelInfo)
+            val elsHintString = generateHintsInML(elsHint, boogieProcAccessor, expRelInfo)
+            MLUtil.app("IfHint", MLUtil.createTuple(Seq(expRelInfo, thnHintString, elsHintString)))
+          case _ => sys.error("if hint has unexpected form")
+        }
       case WhileHint(cond, invs, componentHints) => ???
     }
   }
