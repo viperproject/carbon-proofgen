d53bf13f751704b4ab3d0178fceecad404ad61a2
Andrew <andlee@ethz.ch>
2024-08-14 10:59:50 +0200

    add proof testing scripts

---
+++ scripts/check_proofs_artifact.py
@@ -0,0 +1,111 @@
+import os
+import subprocess
+import time
+import statistics as stat
+import csv
+import argparse
+
+def append_results_to_file(file, times, csv_writer):
+    row = [file,stat.mean(times), stat.median(times), stat.pstdev(times)]
+    csv_writer.writerow(row)
+
+def check_proofs(testsuite_name, input_folder, n_reps):
+
+    print("==============Starting proof checking for {}==============".format(testsuite_name))
+
+    full_start_time = time.time()
+
+    timestr = time.strftime("%Y%m%d_%H%M%S")
+    output_file_name = timestr+"_"+"timing_certificates.csv" 
+    with open(os.path.join(input_folder, output_file_name), 'w', newline='') as output_file:
+        writer = csv.writer(output_file, delimiter=',')
+        writer.writerow(["File","Mean","Median","Pstdev."])
+
+        n_success = 0
+        n_failure = 0
+        for root, dirs, files in os.walk(input_folder):
+            for dir in dirs:
+                root_session_file = os.path.join(root, os.path.join(dir, "ROOT"))
+                if(os.path.isfile(root_session_file)):
+
+                    # build session
+                    print("Checking " + dir)
+                    root_dir = os.path.join(root, dir)                    
+                    times = []
+                    
+                    for i in range(0,n_reps):
+                        start_time = time.time()
+
+                        """
+                        Isabelle options:
+                         * -j4 uses up to 4 cores
+                         * -c cleans previous builds of the same session (otherwise the proofs may not be re-checked)
+                         * `-D root_dir` states that all Isabelle sessions in `root_dir` should be built
+                        """
+                        error_code = subprocess.run(["isabelle", "build", "-j4", "-c", "-D", root_dir], stdout=subprocess.DEVNULL)
+                        duration = time.time() - start_time
+                        times.append(duration)
+                    
+                    append_results_to_file(root_session_file, times, writer)
+
+                    # assume isabelle is deterministic (i.e., sufficient to just check final return code)
+                    if error_code.returncode == 0:
+                        n_success += 1
+                        print("Success")
+                    else:
+                        n_failure += 1
+                        print("Failure")
+
+        full_duration = time.time() - full_start_time
+        print("Finished in " + str(full_duration) + " seconds")
+        if n_failure == 0:
+            print("Isabelle successfully checked all {} files".format(str(n_success)))
+        else:
+            print("Isabelle successfully checked {} files".format(str(n_success)))
+            print("Isabelle failed to check {} files".format(str(n_failure)))
+        
+        return  n_failure == 0, n_success
+
+def main():
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument(
+        "-d", "--inputdir",        
+        help="Specify input directory containing the generated proofs.",        
+        required=True)
+
+    parser.add_argument(
+        "-r", "--reps",
+        help="Specify the number of times the same proof should be checked (for timing purposes).",
+        type=int,
+        default=1)
+
+    args = parser.parse_args()
+
+    if args.reps <= 0:
+        print("Number of repetitions has to be at least 1.")
+        exit(1)
+
+    n_total_success = 0
+    all_success = True
+
+    for testsuite_dir_name in os.listdir(args.inputdir):
+        testsuite_dir = os.path.join(args.inputdir, testsuite_dir_name)
+        testsuite_proofs_dir = os.path.join(testsuite_dir, "proofs_for_eval")
+        if(os.path.exists(testsuite_proofs_dir)):
+            success_testsuite, n_success_testsuite = check_proofs(testsuite_dir_name, testsuite_proofs_dir, args.reps)
+            all_success = all_success and success_testsuite
+            n_total_success += n_success_testsuite
+    
+    print()
+    print()
+    print("========Summary for all test suites========")
+    print()
+    print("Isabelle successfully checked the proofs for {} files in total".format(n_total_success))
+    if all_success:
+        print("Every file was successfully checked")
+    else:
+        print("There were files that could not be successfully checked")
+
+if __name__ == '__main__':
+    main()
---
+++ scripts/generate_proofs_artifact.py
@@ -0,0 +1,135 @@
+import os
+import shutil
+import subprocess
+import argparse
+
+def get_num_vpr_files(input_dir):
+    return len ([f for dp, dn, fn in os.walk(input_dir) for f in fn])
+
+def generate_proofs(testsuite, input_dir, output_dir, carbon_proofgen_bin, boogie_proofgen_bin) -> bool:
+    n_success_verified = 0
+    n_success_not_verified = 0
+    n_not_supported = 0
+    n_unknown_error = 0
+
+    # turn input directory path into an absolute path, since we are going to 
+    # change the working directory
+    input_dir_absolute = os.path.abspath(input_dir)    
+
+    n_vpr_files = get_num_vpr_files(input_dir_absolute)
+
+    print("==============Starting proof generation for {} ({} Viper files)==============".format(testsuite, n_vpr_files))
+
+    if(os.path.exists(output_dir)):
+        print("Removing existing folder {}".format(output_dir))
+        shutil.rmtree(output_dir)
+    
+    # store current work directory such that can change back to it
+    working_dir_at_start = os.getcwd() 
+
+    # change to output directory
+    os.mkdir(output_dir)
+    os.chdir(output_dir)
+
+    for root, dirs, files in os.walk(input_dir_absolute):
+        for file in files:
+            if file.endswith('.vpr'):
+                viper_file_path = os.path.join(root, file)
+
+                with open(viper_file_path) as f:
+                    boogie_file_name = file.split(".vpr")[0]+".cpg.bpl"
+
+                    options = ["--genProofs", "--desugarPolymorphicMaps", "--disableAllocEncoding", "--print", boogie_file_name, "--boogieExe", boogie_proofgen_bin]
+
+                    errorcode = subprocess.run([carbon_proofgen_bin] + options + [viper_file_path], stdout=subprocess.DEVNULL)
+
+                    if(errorcode.returncode == 0):
+                        print("Generated proofs for: {}".format(file))
+                        n_success_verified += 1
+                    elif(errorcode.returncode == 1):
+                        print("Generated proofs for: {}".format(file))
+                        # program does not verify, but proof should be generated
+                        n_success_not_verified += 1
+                    elif(errorcode.returncode == 17):
+                        print("Test not supported by proof generation: " + file)
+                        n_not_supported += 1
+                    else:
+                        print("Unknown error code ({}) for: {}".format(errorcode.returncode, file))
+                        n_unknown_error += 1
+    
+    print()
+
+    n_success = n_success_verified + n_success_not_verified
+    print("Generated proofs for {} tests ({} of these tests are verified by Viper)".format(n_success, n_success_verified))
+
+    if(n_not_supported > 0):
+        print("{} tests are not supported for proof generation".format(n_not_supported))
+
+    if(n_unknown_error > 0):
+        print("{} tests yielded an unknown error code".format(n_unknown_error))
+
+    # check that the reported number of proofs is reflected by the created directories
+    num_proof_dirs = [d for d in os.listdir() if os.path.isdir(d) and d.endswith("proof")]
+    consistent = (len(num_proof_dirs) == n_success)
+    if(not(consistent)):
+        print("INCONSISTENCY: Although {} proofs were reported to be generated, only {} proof directories were created".format(n_success, len(num_proof_dirs)))
+    
+    print()
+    
+    os.chdir(working_dir_at_start)
+
+    return (n_success == n_vpr_files and consistent)
+
+def main():
+    parser = argparse.ArgumentParser(allow_abbrev=False)
+
+    parser.add_argument(
+        "-i", "--inputdir",        
+        help="Directory where all Carbon files are located, for which proofs should be generated.",
+        required=True)
+
+    parser.add_argument(
+        "-v", "--viperproofExe",
+        help="Path to Viper proof generation executable (must be an absolute path or a command that can be executed from any directory)",
+        required=True
+    )
+
+    parser.add_argument(
+        "-b", "--boogieproofExe",
+        help="Path to Boogie proof generation executable (must be an absolute path or a command that can be executed from any directory)",
+        required=True
+    )
+
+    args = parser.parse_args()
+
+    if (not(os.path.isdir(args.inputdir))):
+        print("The input directory " + args.inputdir + " does not point to an existing directory.")
+        exit(1)
+    
+    all_success = True
+
+    for testsuite_dir_name in os.listdir(args.inputdir):
+        testsuite_dir = os.path.join(args.inputdir, testsuite_dir_name)
+        vipertests_dir = os.path.join(testsuite_dir, "vpr_files_for_eval")
+        outputdir = os.path.join(testsuite_dir, "proofs_for_eval")
+        
+        success_testsuite = generate_proofs(testsuite_dir_name,
+                            vipertests_dir, 
+                            outputdir,
+                            args.viperproofExe, 
+                            args.boogieproofExe
+                        )
+        if(not(success_testsuite)):
+            print("Could not proofs for all files in {}".format(testsuite_dir_name))
+        
+        all_success = all_success and success_testsuite
+    
+    if all_success:
+        print()
+        print("Successfully generated proofs for all files")
+    else:
+        print()
+        print("Could not generate proofs for all files")
+
+if __name__ == '__main__':
+    main()
